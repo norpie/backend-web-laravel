cc28ed82af23ebd3b4feb4cc230ef69bf6b828b6
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-27 18:57:00 +0100

    changed config to have postgres

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('POSTGRES_URL', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
