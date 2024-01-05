71f3ab3d6b6359b7eadc306302d9fd457b3d6473
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-05 21:14:32 +0100

    feat: added ideas to navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -22,6 +22,9 @@
             <a class="nav-link" href="/">Home</a>
         </li>
         <li class="nav-item">
+            <a class="nav-link" href="{{ route('ideas.list') }}">Ideas</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="{{ route('showAbout') }}">About</a>
         </li>
         <li class="nav-item">
