a4ed871000e5a7d8a6e7b28b99be33ed408359f5
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-05 15:25:28 +0100

    feat: added block about AI in about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.default')
 
 @section('content')
+    <h1>Websites</h1>
     @include('partials.sources')
+    <h1>Generative AI</h1>
+    <ul>
+        <li><a href="https://openai.com/chatgpt">ChatGPT</a></li>
+        <li><a href="https://github.com/features/copilot">GitHub Copilot</a></li>
+    </ul>
 @endsection
