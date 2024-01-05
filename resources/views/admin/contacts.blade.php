d8da657852e1979cb2c05da328e344ecb056d0b3
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-05 15:52:48 +0100

    refactor: contacts now uses admin layout

--- resources/views/admin/contacts.blade.php
+++ resources/views/admin/contacts.blade.php
@@ -1,20 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Answer Contacts</title>
-</head>
-<body>
+@extends('layouts.admin')
+
+@section('content')
     <h2>Answer Contacts</h2>
     @foreach ($contacts as $contact)
         <p>Name: {{ $contact->name }}</p>
         <p>Email: {{ $contact->email }}</p>
         <p>Message: {{ $contact->message }}</p>
-        <form action="{{ route('admin.respondtocontact', ["id" => $contact->id]) }}" method="POST">
+        <form action="{{ route('admin.respondtocontact', ['id' => $contact->id]) }}" method="POST">
             @csrf
             <textarea name="response" placeholder="Your response" cols="30" rows="10"></textarea>
             <button type="submit">Respond</button>
         </form>
         <hr>
     @endforeach
-</body>
-</html>
+@endsection
