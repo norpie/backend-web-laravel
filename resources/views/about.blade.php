00a85a710d08b8dd592c0eb4e0af171ab1d72d0a
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-05 16:04:57 +0100

    refactor: put AI at the top of about for clarity

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Websites</h1>
-    @include('partials.sources')
     <h1>Generative AI</h1>
     <ul>
         <li><a href="https://openai.com/chatgpt">ChatGPT</a></li>
         <li><a href="https://github.com/features/copilot">GitHub Copilot</a></li>
     </ul>
+
+    <h1>Websites</h1>
+    @include('partials.sources')
 @endsection
