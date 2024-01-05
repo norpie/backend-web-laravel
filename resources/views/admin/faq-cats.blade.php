7cc54b58ddf2f95dc2af7306b9f78d0097288ae5
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-05 15:53:51 +0100

    refactor: faq and faq cats now uses admin layout

--- resources/views/admin/faq-cats.blade.php
+++ resources/views/admin/faq-cats.blade.php
@@ -1,36 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.admin')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Admin News</title>
-</head>
-
-<body>
+@section('content')
     @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
     <div>
         <h2>Existing FAQ Categories</h2>
         <hr>
-        @foreach($faqCats as $faqCat)
-        <form method="POST" action="{{ route('admin.editfaqcat', ['id' => $faqCat->id]) }}">
-            @csrf
-            <label for="name">Name</label><br>
-            <input type="text" name="name" id="name" value="{{ $faqCat->name }}"><br>
-            <button type="submit">Edit</button>
-        </form>
-        <form method="POST" action="{{ route('admin.deletefaqcat', ['id' => $faqCat->id]) }}">
-            @csrf
-            <button type="submit" value="{{ $faqCat->id }}">Delete</button>
-        </form>
-        <hr>
+        @foreach ($faqCats as $faqCat)
+            <form method="POST" action="{{ route('admin.editfaqcat', ['id' => $faqCat->id]) }}">
+                @csrf
+                <label for="name">Name</label><br>
+                <input type="text" name="name" id="name" value="{{ $faqCat->name }}"><br>
+                <button type="submit">Edit</button>
+            </form>
+            <form method="POST" action="{{ route('admin.deletefaqcat', ['id' => $faqCat->id]) }}">
+                @csrf
+                <button type="submit" value="{{ $faqCat->id }}">Delete</button>
+            </form>
+            <hr>
         @endforeach
     </div>
     <div>
@@ -42,6 +36,4 @@
             <button type="submit">Add</button>
         </form>
     </div>
-</body>
-
-</html>
+@endsection
--- resources/views/admin/faq.blade.php
+++ resources/views/admin/faq.blade.php
@@ -1,45 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.admin')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Admin Faq</title>
-</head>
-
-<body>
+@section('content')
     @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
     <div>
         <h2>Existing FAQ</h2>
         <hr>
-        @foreach($faqs as $faq)
-        <form method="POST" action="{{ route('admin.editfaq', ['id' => $faq->id]) }}">
-            @csrf
-            <label for="category">Category</label><br>
-            <select name="category" id="category">
-                <option value="">Select Category</option>
-                @foreach($categories as $category)
-                <option value="{{ $category->id }}" @if($category->id == $faq->category_id) selected @endif>{{ $category->name }}</option>
-                @endforeach
-            </select><br>
-            <label for="question">Question</label><br>
-            <input type="text" name="question" id="question" value="{{ $faq->question }}"><br>
-            <label for="answer">Answer</label><br>
-            <input type="text" name="answer" id="answer" value="{{ $faq->answer }}"><br>
-            <button type="submit">Edit</button>
-        </form>
-        <form method="POST" action="{{ route('admin.deletefaq', ['id' => $faq->id]) }}">
-            @csrf
-            <button type="submit" value="{{ $faq->id }}">Delete</button>
-        </form>
-        <hr>
+        @foreach ($faqs as $faq)
+            <form method="POST" action="{{ route('admin.editfaq', ['id' => $faq->id]) }}">
+                @csrf
+                <label for="category">Category</label><br>
+                <select name="category" id="category">
+                    <option value="">Select Category</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" @if ($category->id == $faq->category_id) selected @endif>
+                            {{ $category->name }}</option>
+                    @endforeach
+                </select><br>
+                <label for="question">Question</label><br>
+                <input type="text" name="question" id="question" value="{{ $faq->question }}"><br>
+                <label for="answer">Answer</label><br>
+                <input type="text" name="answer" id="answer" value="{{ $faq->answer }}"><br>
+                <button type="submit">Edit</button>
+            </form>
+            <form method="POST" action="{{ route('admin.deletefaq', ['id' => $faq->id]) }}">
+                @csrf
+                <button type="submit" value="{{ $faq->id }}">Delete</button>
+            </form>
+            <hr>
         @endforeach
     </div>
     <div>
@@ -49,8 +44,8 @@
             <label for="category">Category</label><br>
             <select name="category" id="category">
                 <option value="">Select Category</option>
-                @foreach($categories as $category)
-                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
                 @endforeach
             </select><br>
             <label for="question">Question</label><br>
@@ -60,6 +55,4 @@
             <button type="submit">Add</button>
         </form>
     </div>
-</body>
-
-</html>
+@endsection
