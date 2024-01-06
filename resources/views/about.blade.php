be389205aa5cb0275cfd03e068c85e04c11d06fc
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-06 10:25:37 +0100

    feat: added github link to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.default')
 
 @section('content')
+    <h1>Github</h1>
+    <p>Check out the <a href="https://github.com/norpie/backend-web-laravel">GitHub repository</a> for this website.</p>
     <h1>Sources</h1>
     <h2>Generative AI</h2>
     <ul>
