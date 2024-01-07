ad22ba75a6fc9ef151ac347f559d44f64d39a942
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-07 19:06:47 +0100

    feat: added styling for faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,36 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.default')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Faq</title>
-</head>
+@section('content')
 
-<body>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+    <style>
+        .container {
+            margin-top: 100px;
+        }
+
+        .container h2 {
+            text-align: center;
+        }
+
+        .container h3 {
+            text-align: center;
+        }
+
+        .container p {
+            text-align: center;
+        }
+
+        .pair {
+            margin: 20px;
+        }
+
+        .question {
+            font-weight: bold;
+        }
+
+        .answer {
+            font-style: italic;
+        }
+
+        .category {
+            font-weight: bold;
+            font-size: 20px;
+        }
+
+    </style>
+    {{-- Separation between questions isn't clear --}}
+    <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div>
+            <h2>FAQ</h2>
+            @foreach ($categories as $category)
+                <h3 class="category">{{ $category->name }}</h3>
+                @foreach ($faqs as $faq)
+                    @if ($faq->category_id == $category->id)
+                        <div class="pair">
+                            <p class="question">{{ $faq->question }}</p>
+                            <p class="answer">{{ $faq->answer }}</p>
+                        </div>
+                    @endif
+                @endforeach
             @endforeach
-        </ul>
+        </div>
     </div>
-    @endif
-    <div>
-        <h2>FAQ</h2>
-        <hr>
-        @foreach($categories as $category)
-        <h3>{{ $category->name }}</h3>
-
-        @foreach($faqs as $faq)
-            @if($faq->category_id == $category->id)
-                <p>{{ $faq->question }}</p>
-                <p>{{ $faq->answer }}</p>
-                <hr>
-            @endif
-        @endforeach
-
-        @endforeach
-    </div>
-</body>
-</html>
+@endsection
